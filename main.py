e284648e1591e8d24f1ff767c06a713597e33eaf
supreeth99799979 <navn18@student.bth.se>
2020-05-30 16:06:31 +0200

    subtraction featured in sub branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from fractions import Fraction
+
 
 app = Flask(__name__)
 
@@ -7,12 +9,23 @@ def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+@app.route('/sub')
+def subtraction():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        C = Fraction(value1)
+        D = Fraction(value2)
+        result = C-D
+        return str(float(result))
 
 
 if __name__ == "__main__":
